82f7925450fb828228e0ba5df15cab3dd6c7ab56
andreapaccagnella94 <andreapacca94@live.it>
2025-07-30 19:57:37 +0200

    add simple html e start js

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 console.log("hi");
 
+//
+/*
+ <div class="col">
+    <div class="card bg-dark" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-4">
+                <img src="./assets/img/female1.png" class="img-fluid rounded-start" alt="...">
+            </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">Card title</h3>
+                        <p class="card-text text-white">This is a wider</p>
+                        <p class="card-text"><small class="text-white">Last updated 3 ago</small></p> mins
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+
+
+*/
+
 // array copiato dallo script su assets
 const teamMembers = [
     {
@@ -41,7 +63,7 @@ const teamMembers = [
 ];
 
 /*
-Dato un array di oggetti rappresentante un team di un’azienda, 
+Dato un array di oggetti rappresentante un team di un’azienda,
 creare una pagina dedicata  in cui mostrare una card per ciascun componente.
 
 (trovate l’array del team all’interno della cartella in allegato)
@@ -49,9 +71,18 @@ creare una pagina dedicata  in cui mostrare una card per ciascun componente.
 Bonus
 
 Rendere l’esercizio responsive, mandando a capo le card
-Aggiungere un form di agginta membri che permetta di visualizzare 
-il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se 
+Aggiungere un form di agginta membri che permetta di visualizzare
+il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se
 volete sentirvi parte del team! 😀)
 
-Buon divertimento e confermate lettura come al solito :babyyoda: 
+Buon divertimento e confermate lettura come al solito :babyyoda:
 */
+
+
+
+
+
+
+
+// selezionare nodi della Dom
+// 
